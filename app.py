c40be606bfe4fe7ee39a0b0b55ef2edfaabe27ae
Anuthth <80307759+Anuthth@users.noreply.github.com>
2025-07-26 00:23:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ TEAM_NAME_MAP = {
     "A.F.C. Bournemouth": "Bournemouth", "Brighton & Hove Albion": "Brighton",
     "Leeds United": "Leeds", "Manchester City": "Man City", "Manchester United": "Man Utd",
     "Newcastle United": "Newcastle", "Nottingham Forest": "Nottm Forest",
-    "Tottenham Hotspur": "Tottenham", "Spurs": "Tottenham", # <-- CHANGE HERE
+    "Tottenham Hotspur": "Tottenham", # <-- CHANGE HERE
     "West Ham United": "West Ham", "Wolverhampton Wanderers": "Wolves",
 }
 # --- Helper and Data Processing Functions ---
