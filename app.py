46780b65e29f723693d5c002dcdbd068247b3d80
Anuthth <80307759+Anuthth@users.noreply.github.com>
2025-08-28 00:19:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -260,7 +260,7 @@ if ratings_df is not None and fixtures_df is not None:
         for col in gw_columns:
             gb.configure_column(col, headerName=col, valueGetter=f"data['{col}'] ? data['{col}'].display : ''", flex=1, minWidth=90, cellStyle=jscode, sortable=True, comparator=JsCode(comparator_template.format(gw_col=col)))
         
-        gb.configure_default_column(resizable=True, sortable=False, filter=False, menuTabs=[])
+        gb.configure_default_column(resizable=True, sortable=True, filter=False, menuTabs=[])
         # FIX: Pass enable_browser_tooltips directly to the AgGrid call
         AgGrid(df_display, gridOptions=gb.build(), allow_unsafe_jscode=True, theme='streamlit-dark', height=(len(df_display) + 1) * 35, key=f'fdr_grid_{start_gw}_{end_gw}', enable_browser_tooltips=True)
 
