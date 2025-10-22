9014dc266fa3077885cf18bfce529c94716db94c
Anuthth <80307759+Anuthth@users.noreply.github.com>
2025-10-22 21:47:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -215,9 +215,8 @@ with st.expander("Glossary & How It Works"):
 ratings_df, fixtures_df = load_data()
 if ratings_df is not None and fixtures_df is not None:
     st.sidebar.header("Controls")
-    start_gw, end_gw = st.sidebar.slider("Select Gameweek Range:", 8, 38, (8, 21))
+    start_gw, end_gw = st.sidebar.slider("Select Gameweek Range:", 1, 38, (1, 8))
     selected_teams = st.sidebar.multiselect("Select teams to display:", PREMIER_LEAGUE_TEAMS, default=PREMIER_LEAGUE_TEAMS)
-
     fh_options = [None] + list(range(start_gw, end_gw + 1))
     free_hit_gw = st.sidebar.selectbox(
         "Select Free Hit Gameweek (optional):",
